1bb5617862cd7244eaedd90a2cb4ad21406c35e6
jacey <jaceygarcia1128@gmail.com>
2025-09-30 10:00:25 -0500

    finished excercise

---
+++ src/main/java/AdressBuilder.java
@@ -0,0 +1,25 @@
+@SuppressWarnings("StringBufferReplaceableByString")
+public class AdressBuilder {
+    public static void main(String[] args) {
+        StringBuilder billing = new StringBuilder();
+        StringBuilder shipping = new StringBuilder();
+
+        billing.append("Billing Street : 123 Main Street ,");
+        billing.append("Billing City : Middleton ,");
+        billing.append("Billing State : TX ,");
+        billing.append("Billing Zip : 75111");
+
+        shipping.append("Shipping Street : 456 Big Sky Blvd ,");
+        shipping.append("Shipping City : Outer Rim ,");
+        shipping.append("Shipping State : TX ,");
+        shipping.append("Shipping Zip : 75333 ");
+
+        String billingFrom = billing.toString();
+        System.out.println(billingFrom);
+
+        String shipTo = shipping.toString();
+        System.out.println(shipTo);
+
+
+    }
+}
